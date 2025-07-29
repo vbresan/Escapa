488ecb4736fadb120feeccbb1151fe617275ee6e
vbresan <288855+vbresan@users.noreply.github.com>
2025-07-29 11:07:48 +0200

    Properly separating debug and release files

--- app/src/main/java/biz/binarysolutions/escapa/MainActivity.java
+++ app/src/main/java/biz/binarysolutions/escapa/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+		setContentView(R.layout.main);
 
 		this.setVolumeControlStream(AudioManager.STREAM_MUSIC);
 		SoundManager.createInstance(this);
--- app/src/withoutAds/java/biz/binarysolutions/escapa/FlavorSpecific.java
+++ app/src/withoutAds/java/biz/binarysolutions/escapa/FlavorSpecific.java
@@ -1,13 +1,45 @@
 package biz.binarysolutions.escapa;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.widget.Button;
+
+import androidx.appcompat.app.AlertDialog;
 
 public class FlavorSpecific {
+
+    private void showSupportDialog() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+
+        builder.setTitle(R.string.SupportDialogTitle);
+        builder.setMessage(R.string.SupportDialogText);
+
+        builder.setPositiveButton(R.string.Continue, (dialog, which) -> {
+
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(activity.getString(R.string.donation_url)));
+            activity.startActivity(intent);
+
+            dialog.dismiss();
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private final Activity activity;
+
     public FlavorSpecific(Activity activity) {
-        // do nothing
+        this.activity = activity;
     }
 
     public void initialize() {
-        // do nothing
+
+        Button button = activity.findViewById(R.id.buttonSupport);
+        if (button != null) {
+            button.setOnClickListener(view -> showSupportDialog());
+        }
     }
 }
